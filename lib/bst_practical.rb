d7f8404de7ea20cead8e50e25cf545ffc6ae7645
seth <sealittl@ucsc.edu>
2018-03-02 21:17:29 -0800

    refactor kth_largest

--- lib/bst_practical.rb
+++ lib/bst_practical.rb
@@ -2,10 +2,10 @@ require_relative "binary_search_tree"
 
 def kth_largest(root, k)
   bst = BinarySearchTree.new(root)
-  until k == 1
+  (k-1).times do
     curr = bst.maximum
     bst.erase(curr)
-    k -= 1
   end
   bst.maximum
+
 end
